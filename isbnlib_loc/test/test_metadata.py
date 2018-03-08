e4371f4c354913f856d9b2d798d10ef5f55dd26f
Alexandre Conde <alexandreconde@hotmail.com>
2018-03-08 11:26:11 +0000

    new test parameters

--- isbnlib_loc/test/test_metadata.py
+++ isbnlib_loc/test/test_metadata.py
@@ -5,16 +5,16 @@
 
 from nose.tools import assert_equals
 from isbnlib import meta
-from .._bnf import query
+from .._loc import query
 
 
 def test_query():
-    """Test the query of metadata (BnF Catalogue Général) with 'low level' queries."""
+    """Test the query of metadata (LoC Library of Congress) with 'low level' queries."""
     assert_equals(len(repr(query('9781849692341'))) == 2, True)
     assert_equals(len(repr(query('9781849692343'))) == 2, True)
 
-    assert_equals(len(repr(query('9782072693144'))) > 100, True)
-    assert_equals(len(repr(query('9782247169092'))) > 100, True)
-    assert_equals(len(repr(query('9782210760851'))) > 100, True)
+    assert_equals(len(repr(query('9780321534965'))) > 100, True)
+    assert_equals(len(repr(query('9781118241257'))) > 100, True)
+    assert_equals(len(repr(query('9780425284629'))) > 100, True)
 
     assert_equals(len(repr(query('9780000000'))) == 2, True)
