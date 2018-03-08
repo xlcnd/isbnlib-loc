22cbf25306d0072940efe986fa9d0383cd0f532f
Alexandre Conde <alexandreconde@hotmail.com>
2018-03-08 11:25:35 +0000

    improved parsing

--- isbnlib_loc/_loc.py
+++ isbnlib_loc/_loc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Query the BnF Catalogue Général service for metadata."""
+"""Query the LoC (Library of Congress) service for metadata."""
 
 import logging
 from xml.dom.minidom import parseString
@@ -10,9 +10,9 @@ from isbnlib.dev.webquery import query as wquery
 
 LOGGER = logging.getLogger(__name__)
 UA = 'isbnlib (gzip)'
-SERVICE_URL = 'http://catalogue.bnf.fr/api/SRU?version=1.2'\
-    '&operation=searchRetrieve&query=bib.isbn%20all%20%22{isbn}'\
-    '%22&maximumRecords=1&recordSchema=dublincore'
+SERVICE_URL = 'http://lx2.loc.gov:210/lcdb?version=2.0'\
+    '&operation=searchRetrieve&query=bath.isbn={isbn}'\
+    '&maximumRecords=1&recordSchema=dc'
 
 
 def _get_text(topnode):
@@ -27,33 +27,32 @@ def _get_text(topnode):
 def _clean_title(title):
     """Clean the Title field of some unnecessary annotations."""
     title = title.replace('<', '').replace('>', '')\
-        .replace(': roman', '').split('/')[0]
+        .replace(' :', ':').split('/')[0]
     return title.strip(':.,; ')
 
 
 def _clean_author(author):
     """Clean the Author field of some unnecessary annotations."""
-    author = author.replace('Auteur du texte', '')\
+    author = author.replace('author', '').replace(' :', ':')\
         .split('/')[0].split(';')[0]
     if '(' in author:
         author = author.split(')')[0] + ')'
     return author.strip(':.,; ')
 
 
-def parser_bnf(xml):
-    """Parse the response from the BnF Catalogue Général service (France)."""
+def parser_loc(xml):
+    """Parse the response from the LoC (Library of Congress) service (US)."""
     # handle special case
     if 'numberOfRecords>0<' in xml:
         return {}
-    # parse xml and extract canonical fields
+    # parse xml and extract canonical fields (Dublin Core)
     dom = parseString(xml)
     keys = ('Title', 'Authors', 'Publisher', 'Year', 'Language')
-    fields = ('dc:title', 'dc:creator', 'dc:publisher', 'dc:date',
-              'dc:language')
+    fields = ('title', 'creator', 'publisher', 'date', 'language')
     recs = {}
     try:
         for key, field in zip(keys, fields):
-            nodes = dom.getElementsByTagName("oai_dc:dc")[0]\
+            nodes = dom.getElementsByTagName("srw_dc:dc")[0]\
                 .getElementsByTagName(field)
             txt = '|'.join([_get_text(node) for node in nodes])
             recs[key] = u(txt)
@@ -61,11 +60,11 @@ def parser_bnf(xml):
         recs['Publisher'] = recs['Publisher'].split('|')[0]
         authors = recs['Authors'].split('|')
         recs['Authors'] = [_clean_author(author) for author in authors]
-        recs['Year'] = ''.join(c for c in recs['Year'] if c.isdigit())
+        recs['Year'] = ''.join(c for c in recs['Year'] if c.isdigit())[:4]
         recs['Title'] = _clean_title(recs['Title'])
         recs['Language'] = recs['Language'].split('|')[0]
     except IndexError:
-        LOGGER.debug('Check the parsing for BnF (possible error!)')
+        LOGGER.debug('Check the parsing for LoC (possible error!)')
     return recs
 
 
@@ -84,10 +83,10 @@ def _mapper(isbn, records):
 
 
 def query(isbn):
-    """Query the BnF Catalogue Général service for metadata."""
+    """Query the LoC Library of Congress service for metadata."""
     data = wquery(
-        SERVICE_URL.format(isbn=isbn), user_agent=UA, parser=parser_bnf)
+        SERVICE_URL.format(isbn=isbn), user_agent=UA, parser=parser_loc)
     if not data:  # pragma: no cover
-        LOGGER.debug('No data from BnF Catalogue Général for isbn %s', isbn)
+        LOGGER.debug('No data from LoC (Library of Congress) for isbn %s', isbn)
         return {}
     return _mapper(isbn, data)
