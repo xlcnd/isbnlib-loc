029b993f2b09c13c6c2b2c533d63d38dbe999b15
Alexandre Conde <alexandreconde@hotmail.com>
2019-03-02 09:14:25 +0000

    split tests

--- isbnlib_loc/test/test_metadata.py
+++ isbnlib_loc/test/test_metadata.py
@@ -10,11 +10,17 @@ from .._loc import query
 
 def test_query():
     """Test the query of metadata (LoC Library of Congress) with 'low level' queries."""
-    assert_equals(len(repr(query('9781849692341'))) == 2, True)
-    assert_equals(len(repr(query('9781849692343'))) == 2, True)
-
     assert_equals(len(repr(query('9780321534965'))) > 100, True)
     assert_equals(len(repr(query('9781118241257'))) > 100, True)
     assert_equals(len(repr(query('9780425284629'))) > 100, True)
 
-    assert_equals(len(repr(query('9780000000'))) == 2, True)
+
+def test_query_missing():
+    """Test LoC with 'low level' queries (missing data)."""
+    assert_equals(len(repr(query('9781849692341'))) <= 2, True)
+    assert_equals(len(repr(query('9781849692343'))) <= 2, True)
+
+
+def test_query_wrong():
+    """Test LoC with 'low level' queries (wrong data)."""
+    assert_equals(len(repr(query('9780000000'))) <= 2, True)
