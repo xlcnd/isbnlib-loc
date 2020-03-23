0c32f5021b2de77c91737a39f046dd99e5510578
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-23 12:51:36 +0000

    init v1.0.1

--- isbnlib_loc/__init__.py
+++ isbnlib_loc/__init__.py
@@ -3,9 +3,7 @@
 # pylint: skip-file
 from ._loc import query
 
-__version__ = '0.0.4'
-
-__version__ = '0.1.3'
+__version__ = '1.0.1'
 
 __plugin_main_language__ = 'en'
 __plugin_service_name__ = 'loc'
