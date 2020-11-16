81c992288e6c701c72391272fddbbd5cda4f2243
Alexandre Conde <alexandreconde@hotmail.com>
2020-11-16 11:33:46 +0000

    fix plugin's metadata

--- isbnlib_loc/__init__.py
+++ isbnlib_loc/__init__.py
@@ -5,6 +5,9 @@ from ._loc import query
 
 __version__ = '1.0.1'
 
-__plugin_main_language__ = 'en'
+__plugin_author__ = 'xlcnd'
+__plugin_description__ = 'Metadata for books from Library of Congress'
+__plugin_homepage__ = 'github.com/xlcnd/isbnlib-loc'
+__plugin_service_language__ = 'en'
 __plugin_service_name__ = 'loc'
 __plugin_type__ = 'metadata'
