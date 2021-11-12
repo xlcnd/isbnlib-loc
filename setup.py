0d45664c0290b72401e8f15cf3cec605977c398b
Alexandre Conde <alexandreconde@hotmail.com>
2021-11-12 17:18:22 +0000

    require isbnlib-3.10.9+

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # isort:skip_file
 
 # isbnlib-loc -- an isbnlib plugin for the LoC Library of Congress (US)
-# Copyright (C) 2018  Alexandre Lima Conde
+# Copyright (C) 2018-2021  Alexandre Lima Conde
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -30,7 +30,7 @@ setup(
     download_url='https://github.com/xlcnd/isbnlib-loc/archive/v%s.zip' % __version__,
     packages=['isbnlib_loc/'],
     entry_points={'isbnlib.metadata': ['loc=isbnlib_loc:query']},
-    install_requires=["isbnlib>=3.9.1,<3.11.0"],
+    install_requires=["isbnlib>=3.10.9,<3.11.0"],
     license='LGPL v3',
     description='A plugin for isbnlib that pulls metadata from LoC (Library of Congress US).',
     long_description=open('README.rst').read(),
@@ -40,11 +40,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
